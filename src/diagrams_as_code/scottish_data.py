a18a1243b45364f8d3fb5f879a6a5bdc4895de67
christophercarlon <cmcarleone@gmail.com>
2024-05-11 23:56:13 +0100

    code diagram changes

--- src/diagrams_as_code/scottish_data.py
+++ src/diagrams_as_code/scottish_data.py
@@ -1,17 +1,17 @@
 from diagrams import Diagram, Cluster
 from diagrams.generic.blank import Blank
 
-with Diagram("Record Type Definitions", show=True, direction="TB"):
+with Diagram("Scottish Road Works Register", show=True, direction="TB"):
     with Cluster("000 – Licensing and Information"):
-        rt_000 = Blank("Detail A")
+        rt_000 = Blank("Text")
         
     with Cluster("001 - Activity"):
-        rt_001 = Blank("Detail C")
+        rt_001 = Blank("Text")
         
     with Cluster("002 - Project for Activity"):
-        rt_002 = Blank("Detail E")
+        rt_002 = Blank("Text")
         
     with Cluster("003 - Project"):
-        rt_003 = Blank("Detail G")
+        rt_003 = Blank("Text")
     
     
