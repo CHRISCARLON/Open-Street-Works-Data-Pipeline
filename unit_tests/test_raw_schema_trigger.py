9ba5f6b30e161274015c3fee15619c1d2c7e1aeb
christophercarlon <cmcarleone@gmail.com>
2024-05-28 21:28:45 +0100

    unit test change

--- unit_tests/test_raw_schema_trigger.py
+++ unit_tests/test_raw_schema_trigger.py
@@ -18,7 +18,7 @@ def test_get_raw_data_year_not_january(mock_datetime):
     
 
 @patch('src.street_manager_permit_functions.schema_name_trigger.datetime')
-def test_get_raw_data_year_january(mock_datetime):
+def test_get_raw_data_year_january_later_in_month(mock_datetime):
     # Test case when the current month is January 25 near the end of the month 
     mock_datetime.now.return_value = datetime(2025, 1, 28)
     expected_output = "raw_data_2024"
