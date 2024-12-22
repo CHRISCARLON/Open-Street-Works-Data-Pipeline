544c0ac2e9bdc820fb1c3b117f9fa2ea8eeb66b0
Chris Carlon <cmcarleone@gmail.com>
2024-12-22 17:34:14 +0000

    updates 2024-12-22 - added in new models for permit details

---
+++ dbt/street_manager_street_works_analysis/models/england_overview/usrn_permit_details_england.sql
@@ -0,0 +1,45 @@
+{% set table_alias = 'usrn_permit_details_england_latest' %}
+{{ config(materialized='table', alias=table_alias) }}
+
+WITH combined_permits AS (
+    SELECT 
+        usrn,
+        street_name,
+        highway_authority,
+        permit_reference_number,
+        promoter_organisation,
+        work_category,
+        activity_type,
+        is_ttro_required,
+        is_traffic_sensitive,
+        traffic_management_type_ref,
+        ofgem_electricity_licence,
+        ofgem_gas_licence,
+        ofcom_licence,
+        ofwat_licence
+    FROM {{ ref('in_progress_list_england') }}
+    
+    UNION ALL
+    
+    SELECT 
+        usrn,
+        street_name,
+        highway_authority,
+        permit_reference_number,
+        promoter_organisation,
+        work_category,
+        activity_type,
+        is_ttro_required,
+        is_traffic_sensitive,
+        traffic_management_type_ref,
+        ofgem_electricity_licence,
+        ofgem_gas_licence,
+        ofcom_licence,
+        ofwat_licence
+    FROM {{ ref('completed_list_england') }}
+)
+
+SELECT 
+    *,
+    {{ current_timestamp() }} AS date_processed
+FROM combined_permits
