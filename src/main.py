96fb0244ade4f10b3ee21f6477a66f16a641f193
Chris Carlon <cmcarleone@gmail.com>
2025-01-11 14:08:20 +0000

    updates 2025-01-11 - fixed table creation  errors for os linked identifiers

--- src/main.py
+++ src/main.py
@@ -1,17 +1,17 @@
 from pipeline_launch_scripts import (
-    # sm_monthly_permit_main,
-    # os_open_usrns_main,
+    sm_monthly_permit_main,
+    os_open_usrns_main,
     swa_codes_main,
     os_open_linked_ids_main,
-    # os_open_roads_main,
+    os_open_roads_main,
 )
 
 def main():
-    # sm_monthly_permit_main.main(100000)
-    # os_open_usrns_main.main(100000)
+    sm_monthly_permit_main.main(100000)
+    os_open_usrns_main.main(100000)
     swa_codes_main.main()
     os_open_linked_ids_main.main(200000)
-    # os_open_roads_main.main(100000)
+    os_open_roads_main.main(100000)
 
 if __name__ == "__main__":
     main()
--- src/pipeline_launch_scripts/os_open_linked_ids_main.py
+++ src/pipeline_launch_scripts/os_open_linked_ids_main.py
@@ -10,7 +10,7 @@ from general_functions.creds import secret_name
 
 from os_open_linked_ids.os_open_linked_ids_processor import load_csv_data
 from os_open_linked_ids.get_redirect_url import fetch_redirect_url
-from os_open_linked_ids.create_motherduck_table import create_table
+from os_open_linked_ids.create_motherduck_table import create_table_1, create_table_2, create_table_3
 
 @profile
 def main(batch_limit: int):
@@ -27,23 +27,23 @@ def main(batch_limit: int):
     token = secrets["motherduck_token"]
     database = secrets["motherdb"]
 
-    # 1
-    conn_usrn_uprn = connect_to_motherduck(token, database)
+    # # 1
+    # conn_usrn_uprn = connect_to_motherduck(token, database)
 
-    logger.success("OS USRN to URPN DATA STARTED")
-    create_table(conn_usrn_uprn, schema="os_open_linked_identifiers", name="os_open_linked_identifiers_uprn_usrn_latest")
-    url = fetch_redirect_url(url="https://api.os.uk/downloads/v1/products/LIDS/downloads?area=GB&format=CSV&fileName=lids-2024-12_csv_BLPU-UPRN-Street-USRN-11.zip&redirect")
-    load_csv_data(url, conn_usrn_uprn, batch_limit, schema="os_open_linked_identifiers", name="os_open_linked_identifiers_uprn_usrn_latest")
-    logger.success("OS USRN to UPRN DATA PROCESSED")
+    # logger.success("OS USRN to URPN DATA STARTED")
+    # create_table(conn_usrn_uprn, schema="os_open_linked_identifiers", name="os_open_linked_identifiers_uprn_usrn_latest")
+    # url = fetch_redirect_url(url="https://api.os.uk/downloads/v1/products/LIDS/downloads?area=GB&format=CSV&fileName=lids-2024-12_csv_BLPU-UPRN-Street-USRN-11.zip&redirect")
+    # load_csv_data(url, conn_usrn_uprn, batch_limit, schema="os_open_linked_identifiers", name="os_open_linked_identifiers_uprn_usrn_latest")
+    # logger.success("OS USRN to UPRN DATA PROCESSED")
 
-    if conn_usrn_uprn:
-        conn_usrn_uprn.close()
+    # if conn_usrn_uprn:
+    #     conn_usrn_uprn.close()
 
     # 2
     conn_road_toid_usrn = connect_to_motherduck(token, database)
 
     logger.success("OS ROAD TOID USRN DATA STARTED")
-    create_table(conn_road_toid_usrn, schema = "os_open_linked_identifiers", name="os_open_linked_identifiers_toid_usrn_road_latest")
+    create_table_2(conn_road_toid_usrn, schema = "os_open_linked_identifiers", name="os_open_linked_identifiers_toid_usrn_road_latest")
     url_2 = fetch_redirect_url(url="https://api.os.uk/downloads/v1/products/LIDS/downloads?area=GB&format=CSV&fileName=lids-2024-12_csv_Road-TOID-Street-USRN-10.zip&redirect")
     load_csv_data(url_2, conn_road_toid_usrn, batch_limit, schema = "os_open_linked_identifiers", name="os_open_linked_identifiers_toid_usrn_road_latest")
     logger.success("OS ROAD TOID USRN DATA PROCESSED")
@@ -55,7 +55,7 @@ def main(batch_limit: int):
     conn_usrn_topo = connect_to_motherduck(token, database)
     
     logger.success("OS USRN TOPO TOID DATA STARTED")
-    create_table(conn=conn_usrn_topo, schema = "os_open_linked_identifiers", name="os_open_linked_identifiers_usrn_topo_area_toid_latest")
+    create_table_3(conn=conn_usrn_topo, schema = "os_open_linked_identifiers", name="os_open_linked_identifiers_usrn_topo_area_toid_latest")
     url_3 = fetch_redirect_url(url="https://api.os.uk/downloads/v1/products/LIDS/downloads?area=GB&format=CSV&fileName=lids-2024-12_csv_Street-USRN-TopographicArea-TOID-4.zip&redirect")
     load_csv_data(url_3, conn_usrn_topo, batch_limit, schema = "os_open_linked_identifiers", name="os_open_linked_identifiers_usrn_topo_area_toid_latest")
     logger.success("OS USRN TOPO TOID DATA PROCESSED")
